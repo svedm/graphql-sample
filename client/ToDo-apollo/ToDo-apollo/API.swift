07703318a3733bba0c45db45361952acb3934e42
Svetoslav Karasev <karasevsi@m2.ru>
2020-10-30 18:39:53 +0300

    Update iOS projects: carthage -> smp, Xcode12

--- client/ToDo-apollo/ToDo-apollo/API.swift
+++ client/ToDo-apollo/ToDo-apollo/API.swift
@@ -1,10 +1,14 @@
+// @generated
 //  This file was automatically generated and should not be edited.
 
 import Apollo
+import Foundation
 
 public struct ToDoTaskInput: GraphQLMapConvertible {
   public var graphQLMap: GraphQLMap
 
+  /// - Parameters:
+  ///   - name: Task name
   public init(name: String) {
     graphQLMap = ["name": name]
   }
@@ -21,17 +25,19 @@ public struct ToDoTaskInput: GraphQLMapConvertible {
 }
 
 public final class AddTaskMutation: GraphQLMutation {
-  /// mutation AddTask($task: ToDoTaskInput!) {
-  ///   add(task: $task) {
-  ///     __typename
-  ///     id
-  ///     name
-  ///   }
-  /// }
-  public let operationDefinition =
-    "mutation AddTask($task: ToDoTaskInput!) { add(task: $task) { __typename id name } }"
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    mutation AddTask($task: ToDoTaskInput!) {
+      add(task: $task) {
+        __typename
+        id
+        name
+      }
+    }
+    """
 
-  public let operationName = "AddTask"
+  public let operationName: String = "AddTask"
 
   public var task: ToDoTaskInput
 
@@ -44,11 +50,13 @@ public final class AddTaskMutation: GraphQLMutation {
   }
 
   public struct Data: GraphQLSelectionSet {
-    public static let possibleTypes = ["Mutation"]
+    public static let possibleTypes: [String] = ["Mutation"]
 
-    public static let selections: [GraphQLSelection] = [
-      GraphQLField("add", arguments: ["task": GraphQLVariable("task")], type: .object(Add.selections)),
-    ]
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("add", arguments: ["task": GraphQLVariable("task")], type: .object(Add.selections)),
+      ]
+    }
 
     public private(set) var resultMap: ResultMap
 
@@ -70,13 +78,15 @@ public final class AddTaskMutation: GraphQLMutation {
     }
 
     public struct Add: GraphQLSelectionSet {
-      public static let possibleTypes = ["ToDoTask"]
+      public static let possibleTypes: [String] = ["ToDoTask"]
 
-      public static let selections: [GraphQLSelection] = [
-        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-        GraphQLField("id", type: .nonNull(.scalar(Int.self))),
-        GraphQLField("name", type: .nonNull(.scalar(String.self))),
-      ]
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(Int.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+        ]
+      }
 
       public private(set) var resultMap: ResultMap
 
@@ -121,17 +131,19 @@ public final class AddTaskMutation: GraphQLMutation {
 }
 
 public final class RemoveTaskMutation: GraphQLMutation {
-  /// mutation RemoveTask($id: Int!) {
-  ///   remove(id: $id) {
-  ///     __typename
-  ///     id
-  ///     name
-  ///   }
-  /// }
-  public let operationDefinition =
-    "mutation RemoveTask($id: Int!) { remove(id: $id) { __typename id name } }"
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    mutation RemoveTask($id: Int!) {
+      remove(id: $id) {
+        __typename
+        id
+        name
+      }
+    }
+    """
 
-  public let operationName = "RemoveTask"
+  public let operationName: String = "RemoveTask"
 
   public var id: Int
 
@@ -144,11 +156,13 @@ public final class RemoveTaskMutation: GraphQLMutation {
   }
 
   public struct Data: GraphQLSelectionSet {
-    public static let possibleTypes = ["Mutation"]
+    public static let possibleTypes: [String] = ["Mutation"]
 
-    public static let selections: [GraphQLSelection] = [
-      GraphQLField("remove", arguments: ["id": GraphQLVariable("id")], type: .object(Remove.selections)),
-    ]
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("remove", arguments: ["id": GraphQLVariable("id")], type: .object(Remove.selections)),
+      ]
+    }
 
     public private(set) var resultMap: ResultMap
 
@@ -170,13 +184,15 @@ public final class RemoveTaskMutation: GraphQLMutation {
     }
 
     public struct Remove: GraphQLSelectionSet {
-      public static let possibleTypes = ["ToDoTask"]
+      public static let possibleTypes: [String] = ["ToDoTask"]
 
-      public static let selections: [GraphQLSelection] = [
-        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-        GraphQLField("id", type: .nonNull(.scalar(Int.self))),
-        GraphQLField("name", type: .nonNull(.scalar(String.self))),
-      ]
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(Int.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+        ]
+      }
 
       public private(set) var resultMap: ResultMap
 
@@ -221,27 +237,31 @@ public final class RemoveTaskMutation: GraphQLMutation {
 }
 
 public final class ToDoListQuery: GraphQLQuery {
-  /// query ToDoList {
-  ///   toDoList {
-  ///     __typename
-  ///     id
-  ///     name
-  ///   }
-  /// }
-  public let operationDefinition =
-    "query ToDoList { toDoList { __typename id name } }"
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query ToDoList {
+      toDoList {
+        __typename
+        id
+        name
+      }
+    }
+    """
 
-  public let operationName = "ToDoList"
+  public let operationName: String = "ToDoList"
 
   public init() {
   }
 
   public struct Data: GraphQLSelectionSet {
-    public static let possibleTypes = ["Query"]
+    public static let possibleTypes: [String] = ["Query"]
 
-    public static let selections: [GraphQLSelection] = [
-      GraphQLField("toDoList", type: .nonNull(.list(.nonNull(.object(ToDoList.selections))))),
-    ]
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("toDoList", type: .nonNull(.list(.nonNull(.object(ToDoList.selections))))),
+      ]
+    }
 
     public private(set) var resultMap: ResultMap
 
@@ -263,13 +283,15 @@ public final class ToDoListQuery: GraphQLQuery {
     }
 
     public struct ToDoList: GraphQLSelectionSet {
-      public static let possibleTypes = ["ToDoTask"]
+      public static let possibleTypes: [String] = ["ToDoTask"]
 
-      public static let selections: [GraphQLSelection] = [
-        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-        GraphQLField("id", type: .nonNull(.scalar(Int.self))),
-        GraphQLField("name", type: .nonNull(.scalar(String.self))),
-      ]
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(Int.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+        ]
+      }
 
       public private(set) var resultMap: ResultMap
 
